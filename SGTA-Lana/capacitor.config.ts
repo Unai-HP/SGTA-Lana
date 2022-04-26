4c1ba3436c3edace5c9ca04ba259fdc7d79c2daf
Unai-HP <30558067+Unai-HP@users.noreply.github.com>
2022-04-26 19:43:41 +0200

    First commit

---
+++ SGTA-Lana/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SGTA-Lana',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
