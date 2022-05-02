b19f9532ce02928d39eae56060aabc6e543edf91
Unai-HP <30558067+Unai-HP@users.noreply.github.com>
2022-05-02 21:38:36 +0200

    Scraperra lehenengo funtzinamendua

--- src/scrappingScripts/Puppet.js
+++ src/scrappingScripts/Puppet.js
@@ -1,9 +1,11 @@
+const { unwatchFile } = require("fs");
 const puppeteer = require("puppeteer");
+const fs = require('fs').promises;
 
 class Puppet {
     browser
     page
-    headless = false
+    headless = true
 
     constructor() {
         this.browser = null;
@@ -19,12 +21,11 @@ class Puppet {
         console.log('\tGetting details for: ' + selector);
         const backSelector = ".ysKsp";
 
-        await this.page.waitForTimeout(2000);
-
         await this.page.waitForSelector(selector)
         await this.page.click(selector)
         // Lehenengo aldian bakarrik click bat egin behar da, baina hurrengoak 2
         try {
+            await this.page.waitForSelector(selector)
             await this.page.click(selector)
         } catch (error) {
             console.log("\t\tFirst datails click.")
@@ -35,10 +36,14 @@ class Puppet {
         await this.page.waitForNavigation({
             waitUntil: 'networkidle2',
         });
+        // wait for 1 second
+        await this.page.waitForTimeout(1000);
 
         // Get html
         const html = await this.page.content();
 
+        // wait for 2 seconds
+        await this.page.waitForTimeout(2000);
         await this.page.waitForSelector(backSelector)
         await this.page.click(backSelector)
 
@@ -48,13 +53,19 @@ class Puppet {
     }
 
     async openGoogleMaps(url) {
+        const cookiesString = await fs.readFile('./cookies.json');
+        const cookies = JSON.parse(cookiesString);
+
         this.browser = await puppeteer.launch({ headless: this.headless });
         this.page = await this.browser.newPage();
+        await this.page.setCookie(...cookies);
         await this.page.goto(url);
 
-        await this.page.waitForSelector(".AIC7ge > form:nth-child(2) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1) > div:nth-child(1)"),
-        await this.page.click('.AIC7ge > form:nth-child(2) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1) > div:nth-child(1)'),
         await this.page.waitForSelector("div.m6QErb:nth-child(4)")
+
+        // Cookiak lortzeko
+        // const cookies = await this.page.cookies();
+        // await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
     }
 
     async getPreferenceDirectionsAllHtml(url) {
@@ -69,44 +80,73 @@ class Puppet {
         return htmls;
     }
 
-    async getPreferencedDirectionsByTypeHtml(type, url = '') {
+    async getPreferencedDirectionsByTypeHtml(pref, url = '') {
+        // wait for 2 seconds
+        await this.page.waitForTimeout(2000);
+
+        let html = '';
         if (url !== '') {
             await this.openGoogleMaps(url);
         }
 
-        await this.openPreference(type);
+        console.log("\t\tOpening preferences. Type: " + pref)
+        await this.openPreference(pref);
 
-        const html = await this.page.content();
-        console.log("\t\t" + type + " preferences closed.")
+        // wait for 2 seconds
+        await this.page.waitForTimeout(2000);
+        await this.page.waitForSelector(".m6QErb:nth-child(4)")
+        .then(async () => {
+            html = await this.page.content();
+            console.log("\t\t" + pref + " preferences closed.");
+        })
+        
         await this.closePreferences();
 
-        return html;
+        if (html == '') {
+            this.getDirectionDetailsHtml(pref);
+        } else {
+            return html;
+        }
 
     }
 
     async openPreference(pref) {
+        console.log("\t\tOpening preferences. Preference: " + pref)
         const opt_selector = ".OcYctc > span:nth-child(1)"
+        const bus_selector = "#transit-vehicle-prefer-0"
+        const train_selector = "#transit-vehicle-prefer-2"
+        const tram_selector = "#transit-vehicle-prefer-3"
+        const subway_selector = "#transit-vehicle-prefer-1"
+
+        this.page.waitForNavigation({
+            waitUntil: 'networkidle2',
+        });
+
         await this.page.waitForSelector(opt_selector)
         await this.page.click(opt_selector)
-        await this.page.waitForSelector("div.MlqQ3d:nth-child(2)")
 
         switch (pref) {
             case 'Bus':
-                await this.page.click("#transit-vehicle-prefer-0")
+                console.log("\t\t\tOpening Bus preferences.")
+                await this.page.waitForSelector(bus_selector)
+                await this.page.click(bus_selector) 
                 break;
             case 'Train':
-                await this.page.click("#transit-vehicle-prefer-2")
+                await this.page.waitForSelector(train_selector)
+                await this.page.click(train_selector)
                 break;
             case 'Tram':
-                await this.page.click("#transit-vehicle-prefer-3")
+                await this.page.waitForSelector(tram_selector)
+                await this.page.click(tram_selector)
                 break;
             case 'Subway':
-                await this.page.click("#transit-vehicle-prefer-1")
+                await this.page.waitForSelector(subway_selector)
+                await this.page.click(subway_selector)
                 break;
         }
 
-        // wait 2 seconds
-        await this.page.waitForTimeout(2500);
+        // wait for 2 seconds
+        await this.page.waitForTimeout(2000);
     }
 
     async closePreferences() {  
@@ -115,31 +155,49 @@ class Puppet {
         const train_selector = "#transit-vehicle-prefer-2"
         const tram_selector = "#transit-vehicle-prefer-3"
         const subway_selector = "#transit-vehicle-prefer-1"
+        const opt_selector = ".OcYctc > span:nth-child(1)"
         const close_selector = ".OcYctc > span:nth-child(2)"
+
+        
+        try {
+            await this.page.click(opt_selector)
+        } catch (error) {
+            console.log("\t\tPreferentziak berriro irekiak.")
+        }
+        
 
         await this.page.waitForSelector(bus_selector)
         if (await this.page.$eval(bus_selector, el => el.checked)) {
+            await this.page.waitForSelector(bus_selector)
             await this.page.click(bus_selector)
         }
 
         await this.page.waitForSelector(train_selector)
         if (await this.page.$eval(train_selector, el => el.checked)) {
+            await this.page.waitForSelector(train_selector)
             await this.page.click(train_selector)
         }
 
         await this.page.waitForSelector(tram_selector)
         if (await this.page.$eval(tram_selector, el => el.checked)) {
+            await this.page.waitForSelector(tram_selector)
             await this.page.click(tram_selector)
         }
 
         await this.page.waitForSelector(subway_selector)
         if (await this.page.$eval(subway_selector, el => el.checked)) {
+            await this.page.waitForSelector(subway_selector)
             await this.page.click(subway_selector)
         }
 
         // close preferences
         await this.page.waitForSelector(close_selector)
         await this.page.click(close_selector)
+        
+        await this.page.goBack();
+
+        // wait for 2 seconds
+        await this.page.waitForTimeout(2000);
     }
 
     async close() {
