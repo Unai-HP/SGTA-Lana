8b46796d78a95ef3c56d06d047502a7788543bc2
Asier Ruiz <asiescp@gmail.com>
2022-05-05 13:58:47 +0200

    Hainbat aldaketa

--- src/laguntzaileak/zuzenketak.ts
+++ src/laguntzaileak/zuzenketak.ts
@@ -1,5 +1,12 @@
 export const lekuak = [
         {
+            "izena": "Lusa (1206)",
+            "koord": {
+                "lat": 43.2135194, 
+                "lng": -3.1300091
+            }
+        },
+        {
             "izena": "Lusa (1207)",
             "koord": {
                 "lat": 43.2133491, 
